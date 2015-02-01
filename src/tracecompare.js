6caf196feb871e7ab7ab93dbd2c054655aec4495
Francois Doray <fdoray@chromium.org>
2015-02-01 11:13:37 -0500

    Add buttons to show metric histograms.

--- src/tracecompare.js
+++ src/tracecompare.js
@@ -2,31 +2,31 @@ exports.tracecompare = tracecompare;
 
 function tracecompare(path) {
   var tracecompare = {
-    CreateMetricDimension: CreateMetricDimension,
-    CreateHistogram: CreateHistogram
+    CreateMetricDimension: CreateMetricDimension
   };
 
   // Formatters.
-  var formatNumber = d3.format(",d");
+  var formatNumber = d3.format(',d');
 
   // Constants.
   var kMetricNames = {
-    "a": "duration",
-    "b": "usermode",
-    "c": "system calls",
-    "d": "interrupted",
-    "e": "wait-cpu",
-    "f": "wait-blocked",
-    "g": "timer",
-    "h": "network",
-    "i": "block-device",
-    "j": "user-input"
+    'a': 'duration',
+    'b': 'usermode',
+    'c': 'system calls',
+    'd': 'interrupted',
+    'e': 'wait-cpu',
+    'f': 'wait-blocked',
+    'g': 'timer',
+    'h': 'network',
+    'i': 'block-device',
+    'j': 'user-input'
   };
   var kNumFilters = 2;
   var kNumBuckets = 50;
 
   // Available metrics with their min/max value.
-  var metrics = {};
+  var metricsDict = {};
+  var metricsArray = new Array();
 
   // Filters, dimensions and groups.
   var filters = new Array();
@@ -35,9 +35,9 @@ function tracecompare(path) {
   var groups = new Array();
   var groupAll = new Array();
 
-  // Histograms.
-  var histogramsDict = {};
-  var histograms = new Array();
+  // Charts.
+  var chartsDict = {};
+  var charts = new Array();
 
   // Load data.
   d3.json(path, function(error, data) {
@@ -45,36 +45,39 @@ function tracecompare(path) {
     // Create an artificial metric.
     // TODO: Remove this.
     data.executions.forEach(function(d) {
-      d["b"] = d["a"] * (0.5 + Math.random());
+      d['b'] = d['a'] * (0.5 + Math.random());
     });
 
     // Find available metrics and compute their min/max value.
     data.executions.forEach(function(d) {
       ForEachProperty(d, function(property) {
-        if (property == "samples")
+        if (property == 'samples')
           return;
 
-        if (metrics.hasOwnProperty(property))
+        if (metricsDict.hasOwnProperty(property))
         {
-          var metric = metrics[property];
+          var metric = metricsDict[property];
           metric.min = Math.min(metric.min, d[property]);
           metric.max = Math.max(metric.max, d[property]);
         }
         else
         {
-          metrics[property] = {
-            "min": d[property],
-            "max": d[property]
+          var metric = {
+            'id': property,
+            'name': kMetricNames[property],
+            'min': d[property],
+            'max': d[property]
           };
+          metricsDict[property] = metric;
+          metricsArray.push(metric);
         }
       });
     });
-    ForEachProperty(metrics, function(metricIdentifier) {
-      var metric = metrics[metricIdentifier];
+    metricsArray.forEach(function(metric) {
       metric.bucketSize = (metric.max - metric.min) / kNumBuckets;
     });
 
-    // Create filters.
+    // Create filters and empty arrays to hold dimensions and groups.
     for (var i = 0; i < kNumFilters; ++i)
     {
       filters.push(crossfilter(data.executions));
@@ -83,93 +86,82 @@ function tracecompare(path) {
       groupAll.push(filters[i].groupAll());
     }
 
+    // Create buttons to add metric charts.
+    var metricButtonsData = d3.selectAll('#metric-selector')
+      .selectAll('li')
+      .data(metricsArray, function(metric) { return metric.id; });
+    var metricButtons = metricButtonsData.enter().append('li');
+    metricButtons.text(function(metric) { return metric.name; });
+    metricButtons.on('click', function(d) { CreateMetricDimension(d.id); });
+    metricButtonsData.exit().remove();;
+
     // Show the total.
     d3.selectAll('#total').text(formatNumber(data.executions.length));
   });
 
-  // Returns the identifier of a metric.
-  // @param metric The name of the metric.
-  // @returns The identifier of the metric.
-  function GetMetricIdentifier(metricName)
-  {
-    var identifier;
-    ForEachProperty(kMetricNames, function(look) {
-      if (kMetricNames[look] == metricName)
-        identifier = look;
-    })
-    return identifier;
-  }
-
   // Creates a dimension for the specified metric.
-  // @param metric The name of the metric.
-  // @returns The identifier of the created dimension.
-  function CreateMetricDimension(metricName)
+  // @param metricId The identifier of the metric.
+  // @returns The id of the created dimension.
+  function CreateMetricDimension(metricId)
   {
-    var metricIdentifier = GetMetricIdentifier(metricName);
+    var metric = metricsDict[metricId];
 
     // Check whether the dimension already exists.
-    if (dimensions[0].hasOwnProperty(metricIdentifier))
-      return metricIdentifier;
+    if (dimensions[0].hasOwnProperty(metricId))
+      return metricId;
 
     // Create the dimension for each filter.
     for (var i = 0; i < kNumFilters; ++i)
     {
-      var dimension = filters[i].dimension(function(d) {
-        return d[metricIdentifier];
+      var dimension = filters[i].dimension(function(execution) {
+        return execution[metricId];
       });
-      var group = dimension.group(function(d) {
-        var bucketSize = metrics[metricIdentifier].bucketSize;
-        return Math.floor(d / bucketSize) * bucketSize;
+      var group = dimension.group(function(metricValue) {
+        var bucketSize = metric.bucketSize;
+        return Math.floor(metricValue / bucketSize) * bucketSize;
       });
-      dimensions[i][metricIdentifier] = dimension;
-      groups[i][metricIdentifier] = group;
+      dimensions[i][metricId] = dimension;
+      groups[i][metricId] = group;
     }
 
-    dimensionsProperties[metricIdentifier] = {
-      name: kMetricNames[metricIdentifier],
-      min: metrics[metricIdentifier].min,
-      max: metrics[metricIdentifier].max
+    dimensionsProperties[metricId] = {
+      name: metric.name,
+      min: metric.min,
+      max: metric.max
     };
 
-    return metricIdentifier;
+    // Create the charts.
+    CreateCharts(metricId);
+
+    return metricId;
   }
 
-  /*
-  // Creates a dimension for the specified stack
-  // @param stackId The stack identifier.
-  // @returns The identifier of the created dimension.
-  function CreateStackDimension(stackId)
+  // Creates charts for the specified dimension.
+  // @param The id of the dimension
+  function CreateCharts(dimensionId)
   {
-    // TODO.
-  }
-  */
-
-  // Creates an histogram for the specified dimension.
-  // @param The identifier of the dimension
-  function CreateHistogram(dimensionIdentifier)
-  {
-    // Check whether the histogram already exists.
-    if (histogramsDict.hasOwnProperty(dimensionIdentifier))
+    // Check whether the chart already exists.
+    if (chartsDict.hasOwnProperty(dimensionId))
       return;
 
-    var dimensionProperties = dimensionsProperties[dimensionIdentifier];
+    var dimensionProperties = dimensionsProperties[dimensionId];
 
-    // Create the histograms.
-    var dimensionHistograms = new Array();
+    // Create the charts.
+    var dimensionCharts = new Array();
     for (var i = 0; i < kNumFilters; ++i)
     {
-      dimensionHistograms.push(barChart()
-        .dimension(dimensions[i][dimensionIdentifier])
-        .group(groups[i][dimensionIdentifier])
+      dimensionCharts.push(barChart()
+        .dimension(dimensions[i][dimensionId])
+        .group(groups[i][dimensionId])
         .x(d3.scale.linear()
             .domain([dimensionProperties.min, dimensionProperties.max])
             .rangeRound([0, 10 * kNumBuckets])));
     }
 
-    histogramsDict[dimensionIdentifier] = histograms.length;
-    histograms.push({
+    chartsDict[dimensionId] = charts.length;
+    charts.push({
       name: dimensionProperties.name,
-      histograms: dimensionHistograms
+      charts: dimensionCharts
     });
   }
 
