b5d0ab802e0f28a4a627a077ebf25ec501ee6720
Francois Doray <francois.pierre-doray@polymtl.ca>
2015-02-26 01:27:03 -0500

    Add credits.

--- src/barchart.js
+++ src/barchart.js
@@ -1,3 +1,5 @@
+// The barChart function is highly inspired from the Crossfilter sample
+// available at http://square.github.io/crossfilter/
 function barChart(callback) {
   if (!barChart.id) barChart.id = 0;
   var margin = {top: 10, right: 10, bottom: 20, left: 10},
